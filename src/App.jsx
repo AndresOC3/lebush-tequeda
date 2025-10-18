af91ce1631fba675d1f1ea4bbf449a9035859bde
AndresOC3 <125925355+AndresOC3@users.noreply.github.com>
2025-10-18 11:17:20 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 
 import React, { useEffect, useMemo, useState } from "react";
 import * as XLSX from "xlsx";
-const API={async login(e,p){const r=await fetch("/api/login",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({email:e,password:p})});if(!r.ok)throw new Error("Credenciales incorrectas");return r.json();},async register(u,t){const r=await fetch("/api/users",{method:"POST",headers:{"Content-Type":"application/json",Authorization:"Bearer "+t},body:JSON.stringify(u)});if(!r.ok)throw new Error("No se pudo crear");return r.json();},async meFetch(u,o={}){const t=localStorage.getItem("token");const h=Object.assign({"Content-Type":"application/json",Authorization:"Bearer "+t},o.headers||{});const r=await fetch(u,{...o,headers:h});if(r.status===401)throw new Error("No autorizado");return r;}};
+const API={async login(e,p){const r=await fetch("/api/login",{method:"POST",headers:{"Content-Type":"application/json"},body: JSON.stringify({
+  correo: e,  
+  password: p
+})});if(!r.ok)throw new Error("Credenciales incorrectas");return r.json();},async register(u,t){const r=await fetch("/api/users",{method:"POST",headers:{"Content-Type":"application/json",Authorization:"Bearer "+t},body:JSON.stringify(u)});if(!r.ok)throw new Error("No se pudo crear");return r.json();},async meFetch(u,o={}){const t=localStorage.getItem("token");const h=Object.assign({"Content-Type":"application/json",Authorization:"Bearer "+t},o.headers||{});const r=await fetch(u,{...o,headers:h});if(r.status===401)throw new Error("No autorizado");return r;}};
 const STATES=["PorHacer","Iniciado","Finalizado","Entregado"];const DOT={PorHacer:"d-por",Iniciado:"d-ini",Finalizado:"d-fin",Entregado:"d-ent"};const uid=()=>Math.random().toString(36).slice(2,9);
 export default function App(){
   const [user,setUser]=useState(()=>JSON.parse(localStorage.getItem("user")||"null"));
