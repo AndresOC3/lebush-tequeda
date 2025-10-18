ef932c9e1d36e7bd18e36b696e804ae51c99b205
AndresOC3 <125925355+AndresOC3@users.noreply.github.com>
2025-10-18 16:27:01 -0600

    Refactor vite.config.js for better configuration
    Refactor Vite configuration to use defineConfig and add build options.

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,23 @@
-export default { server:{ port:5173, proxy:{ '/api':{target:'http://localhost:5000', changeOrigin:true}, '/uploads':{target:'http://localhost:5000', changeOrigin:true} } } };
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Render usará esta carpeta
+  },
+  server: {
+    port: process.env.PORT || 5173,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+})
