0629733c1337ab6bcb689eb4281917052c86966b
AndresOC3 <125925355+AndresOC3@users.noreply.github.com>
2025-10-18 16:36:22 -0600

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Render usará esta carpeta
+    outDir: 'dist',
   },
   server: {
     port: process.env.PORT || 5173,
